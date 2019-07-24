78f8eb80d62a1c07f382094773f11589f56690ee
Samuel Garcia <garci1117@csusm.edu>
2019-07-24 12:01:42 -0700

    Java Lesson

---
+++ weLearn/M2-Javascript/L1-JSintro/script.js
@@ -0,0 +1,14 @@
+console.log("script running");
+
+var name = "Charlie ";
+console.log("Helllo "+ name +"!");
+var age = 15;
+console.log("You are  "+ age + " years old!")
+
+age = randomNumber( 1,19 );
+
+if (age >= 16){
+console.log("I can drive")
+} else{
+  console.log("I cannot drive")
+}
---
+++ weLearn/M2-Javascript/L2-JSfunction/Console/script.js
@@ -0,0 +1,22 @@
+const greetPeople = (name1, name2) => {
+  console.log("Hello, " + name1 + " and " + name2 + "!");
+}
+const getFullName = (firstName , lastName) =>{
+   return firstname + lastName;
+}
+
+var radius = 10;
+var area = Math.PI * radius * radius;
+radius = radius * 2;
+area = Math.PI * Math.pow (radius, 2);
+var width = Math.round(Math.random() * 100);
+var height = Math.
+}
+
+let  n = 1;
+
+const increaseNumber = () =>{
+  n = n + 1;
+  console.log(n);
+}
+setInterval(increaseNumber, 1000);
