8ef642d5b09a0052bee0755117d6dcd72c800f1e
Samuel Garcia <garci1117@csusm.edu>
2019-07-25 12:08:15 -0700

    L4 Loops

---
+++ weLearn/M2-Javascript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,20 @@
+const article = {
+  name: "Dog family gives birth to 10 puppies",
+  views: 1035,
+  datePublished: "03/25/2018",
+author: {
+  name: "Joe Corgi",
+  title: "Senior Canine Editor"
+  }
+};
+
+
+const favortieFood ={
+  fav1: "Pizza ",
+  fav2: "ice cream ",
+  fav3: "Choclate",
+  fav4: "eggs",
+  fav5: "hamburger",
+};
+
+var x =[1,2,3,4,5]
