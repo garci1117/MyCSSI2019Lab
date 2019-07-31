8ed89535225d09a6710c051cf903f3c69bd7d8a7
Samuel Garcia <garci1117@csusm.edu>
2019-07-31 15:57:38 -0700

    python and appmaster

--- weLearn/M3-Python/L2-Python_list_dict/list.py
+++ weLearn/M3-Python/L2-Python_list_dict/list.py
@@ -19,14 +19,18 @@ for index in range(len(smith_siblings)):
     smith_siblings[index] = smith_siblings[index] + " Smith"
 print(smith_siblings)
 
-superheroes = ["Captain", "Wonder Woman", "Storm", "Kamala Khan", "BumbleBee", "Jessica Jones"]
-demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
-if demoted_hero in superheroes:
-    superheroes.remove(demoted_hero)
-    print("Top 5 heroes:", superheroes)
-else:
-    print("Hero not found.")
+#superheroes = ["Captain", "Wonder Woman", "Storm", "Kamala Khan", "BumbleBee", "Jessica Jones"]
+#demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
+#if demoted_hero in superheroes:
+    #superheroes.remove(demoted_hero)
+    #print("Top 5 heroes:", superheroes)
+#else:
+    #print("Hero not found.")
 
 names = ["Rickon", "Bran", "Arya", "Sans", "Jon", "Robb"]
 print(names[::-1])
 print(names[4:2:-1])
+
+states ={"NY": "New York","CA": "California", "TX": "Texas"}
+for abbreviation in states:
+    print(abbreviation + " is short for " + states[abbreviation])
